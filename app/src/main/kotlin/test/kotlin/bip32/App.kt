ce78f1f42fddb2175db8985e907ad1e9f7daa55c
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-01-23 00:57:34 +0300

    derive

--- app/src/main/kotlin/test/kotlin/bip32/App.kt
+++ app/src/main/kotlin/test/kotlin/bip32/App.kt
@@ -1,6 +1,7 @@
 package test.kotlin.bip32
 
 import java.io.ByteArrayOutputStream
+import java.io.OutputStream
 import java.math.BigInteger
 import java.security.Key
 import java.security.MessageDigest
@@ -11,6 +12,7 @@ import javax.crypto.Mac
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 import javax.crypto.spec.SecretKeySpec
+import org.bouncycastle.crypto.digests.RIPEMD160Digest
 import org.bouncycastle.jce.ECNamedCurveTable
 import org.bouncycastle.jce.spec.ECParameterSpec
 import sp.kx.bytes.writeBytes
@@ -106,8 +108,116 @@ private fun getKeyHash(encoded: ByteArray): ByteArray {
     return sha256.digest()
 }
 
+private fun ByteArray.takeLastBytes(n: Int): ByteArray {
+    val size = size
+//    if (size == n) return this
+    val bytes = ByteArray(n)
+    val started = size - n
+    for (index in 0 until n) {
+        bytes[index] = this[index + started]
+    }
+    return bytes
+}
+
+private fun derive(secretKey: ByteArray, publicKey: ByteArray, chainCode: ByteArray, childIndex: Int): Pair<ByteArray, ByteArray> {
+    // If not (normal child): let I = HMAC-SHA512(Key = cpar, Data = serP(point(kpar)) || ser32(i)).
+    val data = ByteArrayOutputStream().use { stream ->
+        stream.write(publicKey)
+        stream.writeBytes(childIndex)
+        stream.toByteArray()
+    }
+    val mac = Mac.getInstance("hmacsha512")
+    val key: Key = SecretKeySpec(chainCode, mac.algorithm)
+    mac.init(key)
+    val I = mac.doFinal(data)
+    check(I.size == 64)
+    val IL = I.copyOfRange(fromIndex = 0, toIndex = 32)
+    val IR = I.copyOfRange(fromIndex = 32, toIndex = 64)
+    val spec: ECParameterSpec = ECNamedCurveTable.getParameterSpec("secp256k1")
+    val ki = BigInteger(1, IL)
+        .add(BigInteger(1, secretKey))
+        .mod(spec.n)
+        .toByteArray()
+        .takeLastBytes(32)
+    check(ki.size == 32) { "ki:size: ${ki.size}" }
+    return ki to IR
+}
+
+
+private fun OutputStream.write32bit(value: Long) {
+    write(value.shr(24).toInt())
+    write(value.shr(16).toInt())
+    write(value.shr(8).toInt())
+    write(value.toInt())
+}
+
+private fun derive02(secretKey: ByteArray, chainCode: ByteArray, childIndex: Int): Pair<ByteArray, ByteArray> {
+    // If so (hardened child): let I = HMAC-SHA512(Key = cpar, Data = 0x00 || ser256(kpar) || ser32(i)).
+    // (Note: The 0x00 pads the private key to make it 33 bytes long.)
+    val data = ByteArrayOutputStream().use { stream ->
+        stream.write(0)
+        stream.write(secretKey)
+        stream.write32bit(childIndex + 0x80000000)
+        stream.toByteArray()
+    }
+    check(data.size == 37) { "data:size: ${data.size}" }
+    val mac = Mac.getInstance("hmacsha512")
+    val key: Key = SecretKeySpec(chainCode, mac.algorithm)
+    mac.init(key)
+    val I = mac.doFinal(data)
+    check(I.size == 64)
+    val IL = I.copyOfRange(fromIndex = 0, toIndex = 32)
+    val IR = I.copyOfRange(fromIndex = 32, toIndex = 64)
+    // The returned child key ki is parse256(IL) + kpar (mod n).
+    val spec: ECParameterSpec = ECNamedCurveTable.getParameterSpec("secp256k1")
+    val ki = BigInteger(1, IL)
+        .add(BigInteger(1, secretKey))
+        .mod(spec.n)
+        .toByteArray()
+        .takeLastBytes(32)
+    check(ki.size == 32) { "ki:size: ${ki.size}" }
+    val sha256 = MessageDigest.getInstance("sha256")
+    val hash = sha256.digest(secretKey)
+    val h160 = RIPEMD160Digest()
+    h160.update(hash, 0, hash.size)
+    val fingerprint = ByteArray(20)
+    h160.doFinal(fingerprint, 0)
+    val message = """
+        fingerprint(${fingerprint.size}): ${fingerprint.hex()}
+    """.trimIndent()
+    println(message)
+    return ki to IR
+}
+
+private fun derive01(secretKey: ByteArray, chainCode: ByteArray, childNumber: Int) {
+    val mac = Mac.getInstance("hmacsha512")
+    val key: Key = SecretKeySpec(chainCode, mac.algorithm)
+    mac.init(key)
+    val seed = ByteArrayOutputStream().use { stream ->
+        stream.writeBytes(secretKey)
+        stream.writeBytes(childNumber)
+        stream.toByteArray()
+    }
+    val I = mac.doFinal(seed)
+    check(I.size == 64)
+    val IL = I.copyOfRange(fromIndex = 0, toIndex = 32)
+    val IR = I.copyOfRange(fromIndex = 32, toIndex = 64)
+    val message = """
+        childNumber: $childNumber
+        IL(${IL.size}): ${IL.hex()}
+        IR(${IR.size}): ${IR.hex()}
+    """.trimIndent()
+    println(message)
+}
+
+private fun getPublicKey(secretKey: ByteArray): ByteArray {
+    val spec: ECParameterSpec = ECNamedCurveTable.getParameterSpec("secp256k1")
+    val point = spec.g.multiply(BigInteger(1, secretKey))
+    return point.getEncoded(true)
+}
+
 fun main() {
-    val mnemonic = "foo"
+    val mnemonic = "fan habit farm amount quarter race already real symbol nothing range adjust"
     val passphrase = "bar"
     val seed = getSeed(mnemonic = mnemonic, passphrase = passphrase)
     val masterKey = getMasterKey(seed = seed)
@@ -115,35 +225,58 @@ fun main() {
     // Use parse256(IL) as master secret key, and IR as master chain code.
     val secretKey = masterKey.copyOfRange(fromIndex = 0, toIndex = 32)
     val chainCode = masterKey.copyOfRange(fromIndex = 32, toIndex = 64)
-    // todo
-    val spec: ECParameterSpec = ECNamedCurveTable.getParameterSpec("secp256k1")
-    val point = spec.g.multiply(BigInteger(1, secretKey))
-    val publicKey = point.getEncoded(true)
+    val publicKey = getPublicKey(secretKey = secretKey)
     // Extended public and private keys are serialized as follows:
-    val extendedPrivateKey = getExtendedKey(
-        version = 0x0488ade4,
-        depth = 0,
-        fingerprint = 0,
-        childNumber = 0,
+//    val extendedPrivateKey = getExtendedKey(
+//        version = 0x0488ade4,
+//        depth = 0,
+//        fingerprint = 0,
+//        childNumber = 0,
+//        chainCode = chainCode,
+//        encoded = secretKey,
+//    )
+//    val extendedPublicKey = getExtendedKey(
+//        version = 0x0488b21e,
+//        depth = 0,
+//        fingerprint = 0,
+//        childNumber = 0,
+//        chainCode = chainCode,
+//        encoded = publicKey,
+//    )
+    val m0n = derive(
+        secretKey = secretKey,
+        publicKey = getPublicKey(secretKey = secretKey),
         chainCode = chainCode,
-        encoded = secretKey,
+        childIndex = 0,
     )
-    val extendedPublicKey = getExtendedKey(
-        version = 0x0488b21e,
-        depth = 0,
-        fingerprint = 0,
-        childNumber = 0,
+    val m0h = derive02(
+        secretKey = secretKey,
         chainCode = chainCode,
-        encoded = publicKey,
+        childIndex = 0,
+    )
+    val m00n = derive(
+        secretKey = m0h.first,
+        publicKey = getPublicKey(secretKey = m0h.first),
+        chainCode = m0h.second,
+        childIndex = 0,
+    )
+    val m00h = derive02(
+        secretKey = m0h.first,
+        chainCode = m0h.second,
+        childIndex = 0,
     )
     val message = """
-        secret:key: ${secretKey.hex()}
-        chain:code: ${chainCode.hex()}
+        secret:key(${secretKey.size}): ${secretKey.hex()}
+        chain:code(${chainCode.size}): ${chainCode.hex()}
         public:key(${publicKey.size}): ${publicKey.hex()}
-        extended:private:key(${extendedPrivateKey.size}): ${extendedPrivateKey.hex()}
-        extended:private:key:checksum: ${getKeyHash(extendedPrivateKey).copyOf(4).hex()}
-        extended:public:key(${extendedPublicKey.size}): ${extendedPublicKey.hex()}
-        extended:public:key:checksum: ${getKeyHash(extendedPublicKey).copyOf(4).hex()}
+        m/0           :key: ${m0n.first.hex()}
+        m/0    :chain:code: ${m0n.second.hex()}
+        m/0'          :key: ${m0h.first.hex()}
+        m/0'   :chain:code: ${m0h.second.hex()}
+        m/0'/0        :key: ${m00n.first.hex()}
+        m/0'/0 :chain:code: ${m00n.second.hex()}
+        m/0'/0'       :key: ${m00h.first.hex()}
+        m/0'/0':chain:code: ${m00h.second.hex()}
     """.trimIndent()
     println(message)
 }
